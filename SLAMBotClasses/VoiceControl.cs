4b77e1fc935113dd8df295a040f76646e7568776
Blargansmarf <joshpicarazzi@gmail.com>
2014-03-19 11:54:18 -0500

    Starting Rizzy Code
    code will start

---
+++ SLAMBotClasses/VoiceControl.cs
@@ -0,0 +1,210 @@
+﻿//Josh Picarazzi is fixing this
+//ooooo more comments
+//comment
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SLAMBotClasses;
+using System.Net;
+using System.Threading;
+using Microsoft.Kinect;
+using System.Windows.Documents;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.AudioFormat;
+
+namespace SLAMBotClasses
+{
+    public class VoiceControl
+    {
+        #region members
+
+        public event EventHandler<VoiceArgs> voiceCommandHeard;
+        private KinectSensor kinectSensor;
+        private DateTime lastSend;
+        private double setRightPower;
+        private double setLeftPower;
+        private SpeechRecognitionEngine sre;
+
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region HelperClasses
+
+        public class VoiceArgs : EventArgs
+        {
+            public double rightPower;
+            public double leftPower;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Starts up the SkeletonSlam class.
+        /// </summary>
+        public VoiceControl()
+        {
+            kinectSensor = KinectSensor.KinectSensors[0];
+
+            sre = CreateSpeechRecognizer();
+
+            kinectSensor.Start();
+
+            sre.SetInputToAudioStream(kinectSensor.AudioSource.Start(),
+                 new SpeechAudioFormatInfo(
+                 EncodingFormat.Pcm, 16000, 16, 1,
+                 32000, 2, null));
+
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+
+
+
+            reset();
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void reset()
+        {
+            lastSend = DateTime.Now;
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static RecognizerInfo GetKinectRecognizer()
+        {
+            Func<RecognizerInfo, bool> matchingFunc = r =>
+            {
+                string value;
+                r.AdditionalInfo.TryGetValue("Kinect", out value);
+                return "True".Equals(value, StringComparison.InvariantCultureIgnoreCase) && "en-US".Equals(r.Culture.Name, StringComparison.InvariantCultureIgnoreCase);
+            };
+            return SpeechRecognitionEngine.InstalledRecognizers().Where(matchingFunc).FirstOrDefault();
+        }
+
+        //here is the fun part: create the speech recognizer
+        private SpeechRecognitionEngine CreateSpeechRecognizer()
+        {
+            // Select a speech recognizer that supports English.
+            //RecognizerInfo info = GetKinectRecognizer();
+            RecognizerInfo info = null;
+            foreach (RecognizerInfo ri in SpeechRecognitionEngine.InstalledRecognizers())
+            {
+                if (ri.Culture.TwoLetterISOLanguageName.Equals("en"))
+                {
+                    info = ri;
+                    break;
+                }
+            }
+            if (info == null)
+                return null;
+            SpeechRecognitionEngine sre;
+            sre = new SpeechRecognitionEngine(info.Id);
+
+            //Now we need to add the words we want our program to recognise
+            var grammar = new Choices();
+            grammar.Add("go");
+            grammar.Add("stop");
+            grammar.Add("turn right");
+            grammar.Add("turn left");
+            //grammar.Add("hello");
+            //Add words here
+
+
+            //set culture - language, country/region
+            var gb = new GrammarBuilder { Culture = info.Culture };
+            gb.Append(grammar);
+
+            //set up the grammar builder
+            var g = new Grammar(gb);
+            sre.LoadGrammar(g);
+
+            //Set events for recognizing, hypothesising and rejecting speech
+            sre.SpeechRecognized += SreSpeechRecognized;
+            //sre.SpeechHypothesized += SreSpeechHypothesized;
+            sre.SpeechRecognitionRejected += SreSpeechRecognitionRejected;
+            return sre;
+        }
+
+        //Speech is recognised
+        private void SreSpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //Very important! - change this value to adjust accuracy - the higher the value
+            //the more accurate it will have to be, lower it if it is not recognizing you
+            if (e.Result.Confidence < 1.0)
+            {
+                RejectSpeech(e.Result);
+            }
+            //and finally, here we set what we want to happen when 
+            //the SRE recognizes a word
+            switch (e.Result.Text.ToUpperInvariant())
+            {
+                case ("GO"):
+                    setRightPower = .5;
+                    setLeftPower = .5;
+                    break;
+                case ("STOP"):
+                    setRightPower = 0;
+                    setLeftPower = 0;
+                    break;
+                case ("TURN RIGHT"):
+                    setRightPower = .8;
+                    setLeftPower = .2;
+                    break;
+                case ("TURN LEFT"):
+                    setRightPower = .2;
+                    setLeftPower = .8;
+                    break;
+                default:
+                    break;
+            }
+
+            TestControls();
+        }
+
+        //if speech is rejected
+        private void RejectSpeech(RecognitionResult result)
+        {
+            /*setRightPower = -.5;
+            setLeftPower = -.5;
+
+            TestControls();*/
+        }
+
+        private void SreSpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            RejectSpeech(e.Result);
+        }
+
+        private void TestControls()
+        {
+            if (voiceCommandHeard != null)
+            {
+                VoiceArgs args = new VoiceArgs();
+                args.rightPower = setRightPower;
+                args.leftPower = setLeftPower;
+                voiceCommandHeard(this, args);
+            }
+        }
+
+        #endregion
+
+    }
+}
